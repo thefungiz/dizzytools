93498da5391cd6e8e6810869455c2dd8c921bfb7
Derek Zeller <root.dzeller@gmail.com>
2018-04-27 14:36:58 -0500

    home looks better now

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class Home extends Component {
 
     render() {
         return (
             <div>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Dizzy Tools</h1>
+                </header>
                 Welcome home
             </div>
         );
